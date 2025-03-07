0e4b07a0bc7065ff99c99acfa74d25b8a2a6039a
sk8rh8r7717 <64029051+sk8rh8r7717@users.noreply.github.com>
2025-03-07 16:05:26 +0500

    Added a GUI

--- script.js
+++ script.js
@@ -36,40 +36,105 @@ function playRound(humanChoice, computerChoice) {
     let calculation = humanVal - computerVal
 
     if (calculation == 0) {
-        console.log('Draw: You both chose ' + humanChoice)
+        msg = "Draw: You both chose " + humanChoice;
         return 0
     }
 
     else if (calculation == -2 || calculation == 1) {
-        console.log('You won, You chose ' + humanChoice + ' and computer chose ' + computerChoice)
+        msg = 'You won, You chose ' + humanChoice + ' and computer chose ' + computerChoice;
+        humanScore++;
         return 1
     }
 
     else if (calculation == -1 || calculation == 2) {
-        console.log('You lost, You chose ' + humanChoice + ' and computer chose ' + computerChoice)
+        msg = 'You lost, You chose ' + humanChoice + ' and computer chose ' + computerChoice;
+        computerScore++;
         return 2
     }
 }
 
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
-    let winner = 0
-    for(let i = 0; i <= 4; i++){
-        winner = playRound(getHumanChoice(), getComputerChoice());
-        if (winner == 1) {
-            humanScore++;
-        }
-        else if (winner == 2) {
-            computerScore++;
-        }
-        console.log(humanScore + " " +  computerScore)
-    }
-
-}
 
 //MAIN
 let humanScore = 0
 let computerScore = 0
+let msg = ""
 
-playGame()
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const messageDiv = document.getElementById("result");
+
+
+
+rockButton.addEventListener("click", playRock);
+paperButton.addEventListener("click", playPaper);
+scissorsButton.addEventListener("click", playScissor);
+function playRock() {
+    winner = playRound("rock", getComputerChoice());
+    let messageListItem = document.createElement("li");
+    messageListItem.innerText = msg + "\n"+ humanScore + " " +  computerScore;
+    messageDiv.append(messageListItem)
+    if (humanScore >= 5 || computerScore >= 5) {
+        rockButton.removeEventListener("click", playRock)
+        paperButton.removeEventListener("click", playPaper)
+        scissorsButton.removeEventListener("click", playScissor)
+        if (humanScore >= 5) {
+            let messageListItem = document.createElement("li");
+            messageListItem.innerText = "You won! score: " + humanScore + " " + computerScore;
+            messageDiv.append(messageListItem);
+        }
+
+        else if (computerScore >= 5) {
+            let messageListItem = document.createElement("li");
+            messageListItem.innerText = "You lost. score: " + humanScore + " " + computerScore;
+            messageDiv.append(messageListItem);
+        }
+    }
+}
+    
+function playPaper() {
+    winner = playRound("paper", getComputerChoice());
+    let messageListItem = document.createElement("li");
+    messageListItem.innerText = msg + "\n"+ humanScore + " " +  computerScore;
+    messageDiv.append(messageListItem)
+    if (humanScore >= 5 || computerScore >= 5) {
+        rockButton.removeEventListener("click", playRock)
+        paperButton.removeEventListener("click", playPaper)
+        scissorsButton.removeEventListener("click", playScissor)
+        if (humanScore >= 5) {
+            let messageListItem = document.createElement("li");
+            messageListItem.innerText = "You won! score: " + humanScore + " " + computerScore;
+            messageDiv.append(messageListItem);
+        }
+
+        else if (computerScore >= 5) {
+            let messageListItem = document.createElement("li");
+            messageListItem.innerText = "You lost. score: " + humanScore + " " + computerScore;
+            messageDiv.append(messageListItem);
+        }
+    }
+}
+    
+function playScissor() {
+    winner = playRound("scissors", getComputerChoice());
+    let messageListItem = document.createElement("li");
+    messageListItem.innerText = msg + "\n"+ humanScore + " " +  computerScore;
+    messageDiv.append(messageListItem)
+    if (humanScore >= 5 || computerScore >= 5) {
+        rockButton.removeEventListener("click", playRock)
+        paperButton.removeEventListener("click", playPaper)
+        scissorsButton.removeEventListener("click", playScissor)
+        if (humanScore >= 5) {
+            let messageListItem = document.createElement("li");
+            messageListItem.innerText = "You won! score: " + humanScore + " " + computerScore;
+            messageDiv.append(messageListItem);
+        }
+
+        else if (computerScore >= 5) {
+            let messageListItem = document.createElement("li");
+            messageListItem.innerText = "You lost. score: " + humanScore + " " + computerScore;
+            messageDiv.append(messageListItem);
+        }
+
+    }
+}
